91e0b005eb6c1110ee76337cd6526372213b3977
Kulezt <ayoluk22@gmail.com>
2026-05-04 17:24:15 +0100

    Updating fetch links to include server path

--- public/login.js
+++ public/login.js
@@ -29,7 +29,7 @@ class LoginValidator {
             const email = this.email.value.trim();
             const password = this.password.value.trim();
 
-            fetch(`/api./tinubu_users?email=${email}`)
+            fetch(`/api/tinubu_users?email=${email}`)
                 .then(response => response.json())
                 .then(users => {
                     
